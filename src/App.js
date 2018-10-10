296e0c997f51cb4b8c92f0e6e19a70ce2954fc65
Adam Raider <adam.raider@wework.com>
2018-10-10 15:44:22 -0400

    its really pretty now

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
-import './App.css';
 import Chart from 'chart.js';
+import color from 'tinycolor2';
 import data from './data';
 
 const APPS = [
@@ -13,19 +13,34 @@ const APPS = [
 ];
 
 const TYPES = {
-  throttled_everything: {},
-  throttle_latency: {},
-  throttle_bw: {},
-  throttle_cpu: {},
-  unthrottle: {}
+  throttled_everything: {
+    label: 'throttle everything'
+  },
+  throttle_latency: {
+    label: 'throttle latency'
+  },
+  throttle_bw: {
+    label: 'throttle bandwidth'
+  },
+  throttle_cpu: {
+    label: 'throttle cpu'
+  },
+  unthrottle: {
+    label: 'unthrottled'
+  }
 };
 
+const LABELS = APPS.map(key => key.replace('_', ' '));
 const COLORS = ['#8cc679', '#c27af7', '#4ea3e4', '#dd9666', '#e8587c'];
 
-const datasets = Object.keys(TYPES).map((type, index) => ({
-  label: type.replace('_', ' '),
-  data: APPS.map(key => data[key][type].mean / 1000),
-  backgroundColor: COLORS[index]
+const DATASETS = Object.keys(TYPES).map((type, index) => ({
+  label: TYPES[type].label,
+  data: APPS.map(key => Math.round((data[key][type].mean / 1000) * 100) / 100),
+  backgroundColor: color(COLORS[index]).setAlpha(0.5),
+  hoverBackgroundColor: color(COLORS[index]).setAlpha(0.5),
+  borderColor: color(COLORS[index]),
+  hoverBorderColor: color(COLORS[index]).darken(),
+  borderWidth: 1
 }));
 
 class App extends Component {
@@ -43,15 +58,17 @@ class App extends Component {
     if (!this.rendered) {
       this.rendered = true;
 
-      const ctx = document.getElementById('myChart').getContext('2d');
-      const labels = APPS.map(key => key.replace('_', ' '));
-      this.myChart = new Chart(ctx, {
+      this.myChart = new Chart(document.getElementById('myChart'), {
         type: 'bar',
         data: {
-          labels,
-          datasets
+          labels: LABELS,
+          datasets: DATASETS
         },
         options: {
+          title: {
+            display: true,
+            text: 'Time to Interactive (TTI) in Seconds'
+          },
           scales: {
             yAxes: [
               {
