6f57b6a350e56ac19dd9cb2086233a17f3e2f32a
Adam Raider <adam.raider@wework.com>
2018-10-10 15:02:00 -0400

    init commit

---
+++ src/App.js
@@ -0,0 +1,80 @@
+import React, { Component } from 'react';
+import './App.css';
+import Chart from 'chart.js';
+import data from './data';
+
+const APPS = [
+  'quincy_building',
+  'quincy_empty',
+  'next_building',
+  'next_empty',
+  'vanilla_building',
+  'vanilla_empty'
+];
+
+const TYPES = {
+  throttled_everything: {},
+  throttle_latency: {},
+  throttle_bw: {},
+  throttle_cpu: {},
+  unthrottle: {}
+};
+
+const COLORS = ['#8cc679', '#c27af7', '#4ea3e4', '#dd9666', '#e8587c'];
+
+const datasets = Object.keys(TYPES).map((type, index) => ({
+  label: type,
+  data: APPS.map(key => data[key][type].mean / 1000),
+  backgroundColor: COLORS[index]
+}));
+
+class App extends Component {
+  state = {
+    width: null,
+    height: null
+  };
+
+  componentDidMount() {
+    this.setState({ width: window.innerWidth, height: window.innerHeight });
+  }
+
+  rendered = false;
+  componentDidUpdate() {
+    if (!this.rendered) {
+      this.rendered = true;
+
+      const ctx = document.getElementById('myChart').getContext('2d');
+      const labels = APPS.map(key => key.replace('_', ' '));
+      this.myChart = new Chart(ctx, {
+        type: 'bar',
+        data: {
+          labels,
+          datasets
+        },
+        options: {
+          scales: {
+            yAxes: [
+              {
+                ticks: {
+                  beginAtZero: true
+                }
+              }
+            ]
+          }
+        }
+      });
+    }
+  }
+
+  render() {
+    return this.state.width ? (
+      <canvas
+        id="myChart"
+        width={this.state.width}
+        height={this.state.height}
+      />
+    ) : null;
+  }
+}
+
+export default App;
---
+++ src/data.js
@@ -0,0 +1,56 @@
+export default Object.freeze({
+  quincy_building: {
+    unthrottle: { mean: 1700.1000000163913, median: 1700.1000000163913 },
+    throttled_everything: {
+      mean: 25521.10000012908,
+      median: 25521.10000012908
+    },
+    throttle_cpu: { mean: 7658.5500000510365, median: 7658.5500000510365 },
+    throttle_bw: { mean: 15009.94999986142, median: 15009.94999986142 },
+    throttle_latency: { mean: 12414.55000010319, median: 12414.55000010319 }
+  },
+  quincy_empty: {
+    unthrottle: { mean: 958.8000000221655, median: 958.8000000221655 },
+    throttled_everything: {
+      mean: 12009.749999968335,
+      median: 12009.749999968335
+    },
+    throttle_cpu: { mean: 4559.449999942444, median: 4559.449999942444 },
+    throttle_bw: { mean: 5234.249999979511, median: 5234.249999979511 },
+    throttle_latency: { mean: 6836.250000051223, median: 6836.250000051223 }
+  },
+  next_building: {
+    unthrottle: { mean: 544.9500000104308, median: 544.9500000104308 },
+    throttled_everything: {
+      mean: 8395.850000088103,
+      median: 8395.850000088103
+    },
+    throttle_cpu: { mean: 1094.950000056997, median: 1094.950000056997 },
+    throttle_bw: { mean: 3072.2999999998137, median: 3072.2999999998137 },
+    throttle_latency: { mean: 5181.600000010803, median: 5181.600000010803 }
+  },
+  next_empty: {
+    unthrottle: { mean: 26.0, median: 26.0 },
+    throttled_everything: {
+      mean: 3794.9499998940155,
+      median: 3794.9499998940155
+    },
+    throttle_cpu: { mean: 443.59999999869615, median: 443.59999999869615 },
+    throttle_bw: { mean: 35.5, median: 35.5 },
+    throttle_latency: { mean: 1024.0, median: 1024.0 }
+  },
+  vanilla_building: {
+    unthrottle: { mean: 266.0, median: 266.0 },
+    throttled_everything: { mean: 3042.5, median: 3042.5 },
+    throttle_cpu: { mean: 391.9499999610707, median: 391.9499999610707 },
+    throttle_bw: { mean: 197.0, median: 197.0 },
+    throttle_latency: { mean: 2928.5, median: 2928.5 }
+  },
+  vanilla_empty: {
+    unthrottle: { mean: 21.0, median: 21.0 },
+    throttled_everything: { mean: 1081.5, median: 1081.5 },
+    throttle_cpu: { mean: 54.0, median: 54.0 },
+    throttle_bw: { mean: 58.0, median: 58.0 },
+    throttle_latency: { mean: 1024.0, median: 1024.0 }
+  }
+});
