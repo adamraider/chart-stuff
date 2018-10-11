bd1c1bab747a8de54e5ab752358c5ded186160dd
Adam Raider <adam.raider@wework.com>
2018-10-11 15:00:17 -0400

    query param stuff

--- src/App.js
+++ src/App.js
@@ -1,19 +1,14 @@
 import React, { Component } from 'react';
 import Chart from 'chart.js';
 import color from 'tinycolor2';
-import data from './data';
+import _ from 'lodash';
+import datafromnajati from './data';
 
-const APPS = [
-  'quincy_building',
-  'quincy_empty',
-  'next_building',
-  'next_empty',
-  'vanilla_building',
-  'vanilla_empty'
-];
-
-const TYPES = {
+const TYPES_MAP = {
   throttled_everything: {
+    label: 'throttle everything'
+  },
+  throttle_everything: {
     label: 'throttle everything'
   },
   throttle_latency: {
@@ -30,67 +25,111 @@ const TYPES = {
   }
 };
 
-const LABELS = APPS.map(key => key.replace('_', ' '));
-const COLORS = ['#8cc679', '#c27af7', '#4ea3e4', '#dd9666', '#e8587c'];
+const COLORS = [
+  '#8cc679',
+  '#c27af7',
+  '#4ea3e4',
+  '#dd9666',
+  '#e8587c',
+  '#6D435A',
+  '#FF6978'
+];
 
-const DATASETS = Object.keys(TYPES).map((type, index) => ({
-  label: TYPES[type].label,
-  data: APPS.map(key => Math.round((data[key][type].mean / 1000) * 100) / 100),
-  backgroundColor: color(COLORS[index]).setAlpha(0.5),
-  hoverBackgroundColor: color(COLORS[index]).setAlpha(0.5),
-  borderColor: color(COLORS[index]),
-  hoverBorderColor: color(COLORS[index]).darken(),
-  borderWidth: 1
-}));
+function parseData(data) {
+  const apps = Object.keys(data);
+  const types = _.sortBy(
+    _.uniq(
+      _.reduce(apps, (memo, app) => memo.concat(Object.keys(data[app])), [])
+    ),
+    type => -_.get(data, `[${apps[0]}][${type}].mean`, 0)
+  );
+  const labels = apps.map(key => key.replace('_', ' '));
+  const datasets = types.map((type, index) => {
+    return {
+      label: _.get(TYPES_MAP, `${type}.label`, type),
+      data: apps
+        .map(
+          key =>
+            data[key][type]
+              ? Math.round((data[key][type].mean / 1000) * 100) / 100
+              : null
+        )
+        .filter(Boolean),
+      backgroundColor: color(COLORS[index]).setAlpha(0.5),
+      hoverBackgroundColor: color(COLORS[index]).setAlpha(0.5),
+      borderColor: color(COLORS[index]),
+      hoverBorderColor: color(COLORS[index]).darken(),
+      borderWidth: 1
+    };
+  });
+
+  return { labels, datasets };
+}
 
 class App extends Component {
-  state = {
-    width: null,
-    height: null
-  };
-
-  componentDidMount() {
-    this.setState({ width: window.innerWidth, height: window.innerHeight });
+  constructor(props) {
+    super(props);
+    const data = decodeURIComponent(
+      new URLSearchParams(window.location.search).get('q')
+    );
+    console.log(data);
+    this.state = {
+      width: null,
+      height: null,
+      data: JSON.parse(data) || datafromnajati
+    };
   }
 
-  rendered = false;
-  componentDidUpdate() {
-    if (!this.rendered) {
-      this.rendered = true;
+  componentDidMount() {
+    this.setState(
+      { width: window.innerWidth, height: window.innerHeight },
+      () => this.doWork()
+    );
+  }
 
-      this.myChart = new Chart(document.getElementById('myChart'), {
-        type: 'bar',
-        data: {
-          labels: LABELS,
-          datasets: DATASETS
-        },
-        options: {
-          title: {
-            display: true,
-            text: 'Time to Interactive (TTI) in Seconds'
-          },
-          scales: {
-            yAxes: [
-              {
-                ticks: {
-                  beginAtZero: true
-                }
-              }
-            ]
-          }
-        }
-      });
+  doWork() {
+    const { labels, datasets } = parseData(this.state.data);
+
+    if (this.myChart) {
+      this.myChart.destroy();
     }
+
+    this.myChart = new Chart(document.getElementById('myChart'), {
+      type: 'bar',
+      data: {
+        labels,
+        datasets
+      },
+      options: {
+        title: {
+          display: true,
+          text: 'Time to Interactive (TTI) in Seconds'
+        },
+        scales: {
+          yAxes: [
+            {
+              ticks: {
+                beginAtZero: true
+              }
+            }
+          ]
+        }
+      }
+    });
   }
 
   render() {
-    return this.state.width ? (
-      <canvas
-        id="myChart"
-        width={this.state.width}
-        height={this.state.height}
-      />
-    ) : null;
+    return (
+      <React.Fragment>
+        {this.state.width ? (
+          <canvas
+            id="myChart"
+            width={this.state.width}
+            height={this.state.height}
+          />
+        ) : null}
+      </React.Fragment>
+    );
   }
 }
 
